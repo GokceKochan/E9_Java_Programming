32bc7d183b72695376dac7f9b49d09da5c3618d5
gokceeksioglu <gokceeksioglu@hotmail.com>
2022-07-30 23:48:33 +0100

    Revise

--- src/WorkOut/Inheritance_day37/workOut/scrumTask/Developer.java
+++ src/WorkOut/Inheritance_day37/workOut/scrumTask/Developer.java
@@ -1,6 +1,6 @@
 package WorkOut.Inheritance_day37.workOut.scrumTask;
 
-public class Developer extends Tester{
+public class Developer extends Employee{
 
     public Developer(String name, int age, char gender, String jobTitle, int id, double salary, String companyName) {
         super(name, age, gender, jobTitle, id, salary, companyName);
